917df48c57aba38e0a16a957e2058b60e0f15aa2
James Goatcher <jamesgoatcher@gmail.com>
2016-06-27 12:49:47 -0700

    CSS essentials for each page is complete.  GET pages for home, signup, login, new post, and show topic are working.  POST still works for signup and login but no sessions yet.

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,7 @@ var router		= express.Router();
 var User 		= require('../models/users.js');
 var bcrypt		= require('bcrypt');
 
-//GET - sign up page
+//GET, INDEX - sign up page
 router.get('/index', function(req, res) {
 	res.render('users/index.ejs')
 })
@@ -14,8 +14,21 @@ router.get('/index', function(req, res) {
 router.post('/', function(req, res) {
 	req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
 	User.create(req.body, function(err, data) {
+		console.log(err);
+		console.log('===');
+		console.log(data);
 		res.redirect('/');
 	})
+})
+
+//GET, NEW - sign up page
+router.get('/new', function(req, res) {
+	res.render('users/new.ejs')
+})
+
+//GET, SHOW - sign up page
+router.get('/show', function(req, res) {
+	res.render('users/show.ejs')
 })
 
 //allows router use
